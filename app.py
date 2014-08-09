6ecb1b2c9b1e90c9e617b788de1a038ebec7956a
Prakhar Bhandari <prakharb@seas.upenn.edu>
2014-08-08 20:47:08 -0400

    removed debugging

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def main(path=''):
     return render_template('index.html', zipped=zip(data, color_values), jobs=flag, next=has_next)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
